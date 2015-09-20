a0011e2f0e2c5e03ae4cb93a7c534baf13952934
Edwin Cesar <edwinpjdcesar@gmail.com>
2015-09-20 17:29:32 -0500

    Google Maps
    Added Google maps functionality

--- js/career.js
+++ js/career.js
@@ -9,16 +9,43 @@
 */
 
 $(document).ready(function () {
-    //  cool animations
-    function doAnimation(element_ID, animation) {
-        $(element_ID).addClass(animation);
-        var wait = window.setTimeout(function () {
-            $(element_ID).removeClass(animation)
-        }, 1300
-        );
-    }
+    mapIt = function () {
+        //  google maps
+        var uwfv;
+        var fvtc;
 
-   // //  google maps
+        uwfv = new GMaps({
+            div: '#uwfvMap',
+            position: 'center',
+            scrollwheel: false,
+            lat: 44.231329,
+            lng: -88.415909,
+            zoom: 10
+        });
+        uwfv.addMarker({
+            lat: 44.231329,
+            lng: -88.415909,
+            title: 'University of Wisconin: Fox Valley'
+        });
+        google.maps.event.trigger(uwfv, 'resize');
+
+        fvtc = new GMaps({
+            div: '#fvtcMap',
+            position: 'center',
+            scrollwheel: false,
+            lat: 44.285734,
+            lng: -88.458309,
+            zoom: 10
+        });
+        fvtc.addMarker({
+            lat: 44.285734,
+            lng: -88.458309,
+            title: 'Fox Valley Technical College'
+        });
+        google.maps.event.trigger(fvtc, 'resize');
+    }
+    
+    
    //var map = new GMaps({
    //     div: '#uwfoxMap',
    //     width: '500px',
@@ -35,6 +62,15 @@ $(document).ready(function () {
    //});
    //google.maps.event.trigger(map, "resize");
 
+    //  cool animations
+    function doAnimation(element_ID, animation) {
+        $(element_ID).addClass(animation);
+        var wait = window.setTimeout(function () {
+            $(element_ID).removeClass(animation)
+        }, 1300
+        );
+    }
+
     $('.petite-heading').addClass('animated', function () {
         doAnimation(this, 'rollIn');
         $('.sub-heading').addClass('animated', function () {
@@ -42,6 +78,7 @@ $(document).ready(function () {
             $('.right, .left').addClass('animated', function () {
                 doAnimation('.right', 'slideInRight');
                 doAnimation('.left', 'slideInLeft');
+                
                 $('.back').addClass('animated', 2000, function () {
                     doAnimation(this, 'fadeIn');
                     $(this).click(function () {
@@ -72,7 +109,43 @@ $(document).ready(function () {
                     $('#s6').animate({ width: '0%' });
                     $('#s7').animate({ width: '0%' });
                 })
+                $('#c3').on('shown.bs.collapse', function () {
+                    mapIt();
+                })
             })
         })
     })
 });
+
+//var uwfv;
+//var fvtc;
+//$(document).ready(function () {
+//    uwfv = new GMaps({
+//        div: '#uwfoxMap',
+//        scrollwheel: false,
+//        lat: 44.231329,
+//        lng: -88.415909,
+//        zoom: 16
+//    });
+//    uwfv.addMarker({
+//        lat: 44.231329,
+//        lng: -88.415909,
+//        title: 'University of Wisconin: Fox Valley',
+//        icon: 'https://cloud.githubusercontent.com/assets/10873565/9982400/bb39cc2a-5fa0-11e5-97f9-d4066ad8946d.jpg'
+//    });
+//});
+//$(document).ready(function () {
+//    fvtc = new GMaps({
+//        div: '#fvtcMap',
+//        scrollwheel: false,
+//        lat: 44.285734,
+//        lng: -88.458309,
+//        zoom: 10
+//    });
+//    fvtc.addMarker({
+//        lat: 44.285734,
+//        lng: -88.458309,
+//        title: 'Fox Valley Technical College',
+//        icon: 'https://cloud.githubusercontent.com/assets/10873565/9982420/6bbc79ee-5fa1-11e5-8b76-35bac592e960.jpg'
+//    });
+//});
